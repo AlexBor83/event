edbf92fe65390b36d4a97203c984bb287931a824
AlexBor83 <rabota_for_alex@mail.ru>
2022-08-13 00:09:40 +0400

    Сделал события

--- script.js
+++ script.js
@@ -1,5 +1,42 @@
+'use strict';
+
+const button = document.getElementById('btn');
+const inputRange = document.getElementById('range');
+const textRange = document.getElementById('range-span');
+const inputText = document.getElementById('text');
+const textSpan = document.getElementById('text-span');
+
+const square = document.getElementById('square');
+const circle = document.getElementById('circle');
+const ebtn = document.getElementById('e_btn');
+
+const log = function () {
+  console.log('rkbr');
+};
+
+const getColor = function () {
+  let color = inputText.value;
+  square.style.backgroundColor = color;
+};
+
+button.addEventListener('click', getColor);
 
 
+textRange.textContent = inputRange.value;
 
+inputRange.addEventListener('input', function (e) {
+  ebtn.style.display = 'none';
+  textRange.textContent = e.target.value;
+
+  circle.style.height = e.target.value + '%';
+  circle.style.width = e.target.value + '%';
+});
+
+console.log(button);
+console.dir(inputRange);
+console.log(textRange);
+console.dir(inputText);
+console.log(textSpan);
+console.dir(square);
 
 
